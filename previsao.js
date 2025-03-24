a4ef70de1d248ac4cbd0ad6fda9ecf2daddbefc2
Ramiro Rockenbach <134063382+ramirotr@users.noreply.github.com>
2025-03-24 11:54:33 -0300

    Primeira
    first

---
+++ previsao.js
@@ -0,0 +1,37 @@
+async function buscarPrevisao() {
+    const cidade = document.getElementById("city").value;
+    const resultado = document.getElementById("resultadoPrevisao");
+    const apiKey = "51efff6ad8e1410550e7181f0aa68879";
+  
+    if (!cidade) {
+      resultado.innerHTML = "<p>Digite uma cidade para buscar a previsão.</p>";
+      return;
+    }
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&lang=pt_br&units=metric`;
+  
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Cidade não encontrada.");
+      }
+  
+      const data = await response.json();
+  
+      const descricao = data.weather[0].description;
+      const temperatura = data.main.temp;
+      const umidade = data.main.humidity;
+      const vento = data.wind.speed;
+  
+      resultado.innerHTML = `
+        <h3>${data.name}</h3>
+        <p><strong>Clima:</strong> ${descricao}</p>
+        <p><strong>Temperatura:</strong> ${temperatura} °C</p>
+        <p><strong>Umidade:</strong> ${umidade}%</p>
+        <p><strong>Vento:</strong> ${vento} m/s</p>
+      `;
+    } catch (erro) {
+      resultado.innerHTML = `<p>Erro: ${erro.message}</p>`;
+    }
+  }
+  
